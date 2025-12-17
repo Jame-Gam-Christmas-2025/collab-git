756a93c1edfc871cd2955243f501eb15a2b77846
shivaladenree <francois.ischia2@gmail.com>
2025-12-17 20:48:45 +0100

    commit soundbank wwise

---
+++ Assets/Wwise/GeneratedSoundBank/Wwise_IDs.h
@@ -0,0 +1,59 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMB_CAVE = 2314402090U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace SW_MC_FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 197902822U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID MC_FOOTSTEPS_GRAVEL = 4054918909U;
+                static const AkUniqueID MC_FOOTSTEPS_LEAF = 2095016128U;
+            } // namespace SWITCH
+        } // namespace SW_MC_FOOTSTEPS
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID MC = 1685527061U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+        static const AkUniqueID MC = 1685527061U;
+        static const AkUniqueID REVERB_CAVE = 323187407U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
